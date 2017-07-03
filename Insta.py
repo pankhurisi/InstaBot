9ea0e971d304a322a6e96d906045e6da6b1c6c9e
pankhurisi <pankhurisingh19ju@gmail.com>
2017-07-03 21:57:11 +0530

    my first commit

---
+++ Insta.py
@@ -0,0 +1,20 @@
+import requests
+APP_ACCESS_TOKEN = '1943018362.461a955.12a586002daf46859def8db521710a5f'
+BASE_URL = 'https://api.instagram.com/v1/'
+
+
+def self_info():
+    request_url = (BASE_URL + 'users/self/?access_token=%s') % (APP_ACCESS_TOKEN)
+    print 'GET request url : %s' % (request_url)
+    user_info = requests.get(request_url).json()
+
+    if user_info['meta']['code'] == 200:
+        if len(user_info['data']):
+            print 'Username: %s' % (user_info['data']['username'])
+            print 'No. of followers: %s' % (user_info['data']['counts']['followed_by'])
+            print 'No. of people you are following: %s' % (user_info['data']['counts']['follows'])
+            print 'No. of posts: %s' % (user_info['data']['counts']['media'])
+        else:
+            print 'User does not exist!'
+    else:
+        print 'Status code other than 200 received!'
