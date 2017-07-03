26fe222eab2f1e4cb3598e90a58d0feb4235c6c5
pankhurisi <pankhurisingh19ju@gmail.com>
2017-07-04 00:16:18 +0530

    user info fetched

--- Insta.py
+++ Insta.py
@@ -4,6 +4,7 @@ APP_ACCESS_TOKEN = '1943018362.461a955.12a586002daf46859def8db521710a5f'
 BASE_URL = 'https://api.instagram.com/v1/'
 
 
+# Function declaration for self info
 def self_info():
     request_url = (BASE_URL + 'users/self/?access_token=%s') % APP_ACCESS_TOKEN
     print 'GET request url : %s' % request_url
@@ -21,6 +22,7 @@ def self_info():
         print 'Status code other than 200 received!'
 
 
+# Function declaration for getting other user's id by username
 def get_user_id(insta_username):
     request_url = (BASE_URL + 'users/search?q=%s&access_token=%s') % (insta_username, APP_ACCESS_TOKEN)
     print 'GET request url : %s' % (request_url)
@@ -34,3 +36,26 @@ def get_user_id(insta_username):
     else:
         print 'Status code other than 200 received!'
         exit()
+
+
+# Function declaration to get information of a user by username
+def get_user_info(insta_username):
+    user_id = get_user_id(insta_username)
+    if user_id == None:
+        print 'User does not exist!'
+        exit()
+    request_url = (BASE_URL + 'users/%s?access_token=%s') % (user_id, APP_ACCESS_TOKEN)
+    print 'GET request url : %s' % (request_url)
+    user_info = requests.get(request_url).json()
+
+    if user_info['meta']['code'] == 200:
+        if len(user_info['data']):
+            print 'Username: %s' % (user_info['data']['username'])
+            print 'No. of followers: %s' % (user_info['data']['counts']['followed_by'])
+            print 'No. of people you are following: %s' % (user_info['data']['counts']['follows'])
+            print 'No. of posts: %s' % (user_info['data']['counts']['media'])
+        else:
+            print 'There is no data for this user!'
+    else:
+        print 'Status code other than 200 received!'
+
