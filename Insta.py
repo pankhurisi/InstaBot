95bb0f53e98d4c15a174ddaae0c2818fe9e2e9bb
pankhurisi <pankhurisingh19ju@gmail.com>
2017-07-03 22:56:59 +0530

    user_id fetched

--- Insta.py
+++ Insta.py
@@ -1,11 +1,12 @@
 import requests
 APP_ACCESS_TOKEN = '1943018362.461a955.12a586002daf46859def8db521710a5f'
+# Token Owner:pankhurisingh19ju
 BASE_URL = 'https://api.instagram.com/v1/'
 
 
 def self_info():
-    request_url = (BASE_URL + 'users/self/?access_token=%s') % (APP_ACCESS_TOKEN)
-    print 'GET request url : %s' % (request_url)
+    request_url = (BASE_URL + 'users/self/?access_token=%s') % APP_ACCESS_TOKEN
+    print 'GET request url : %s' % request_url
     user_info = requests.get(request_url).json()
 
     if user_info['meta']['code'] == 200:
@@ -18,3 +19,18 @@ def self_info():
             print 'User does not exist!'
     else:
         print 'Status code other than 200 received!'
+
+
+def get_user_id(insta_username):
+    request_url = (BASE_URL + 'users/search?q=%s&access_token=%s') % (insta_username, APP_ACCESS_TOKEN)
+    print 'GET request url : %s' % (request_url)
+    user_info = requests.get(request_url).json()
+
+    if user_info['meta']['code'] == 200:
+        if len(user_info['data']):
+            return user_info['data'][0]['id']
+        else:
+            return None
+    else:
+        print 'Status code other than 200 received!'
+        exit()
